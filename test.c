45272651e064f6a5ff01851c9904dc2f5323e829
Unknown <michal.mjk1@gmail.com>
2018-04-22 00:54:45 +0200

    ww

--- test.c
+++ test.c
@@ -1,38 +1,110 @@
-#include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
-#include <signal.h>
-#include <sys/mman.h>
-#include <string.h>
-#define MAX_PIDS 32
+#include <stdio.h>
+#include <stdlib.h>
+
+#define WITH_SIGNALS
+
+#ifdef WITH_SIGNALS
+
+int set_signal_parent = 0;
+
+void parent_handler(int signum) {
+    set_signal_parent = 1;
+	printf("\nparent[%d]: Signal SIGINT caught.\n", getpid());
+
+ }
+
+ void child_handler(int signum) {
+	printf("child[%d]: SIGTERM caught.\n", getpid());
+}
+#endif // WITH_SIGNAL
+
+
+
+void child_function(){
+
+    #ifdef WITH_SIGNALS
+	signal(SIGINT, SIG_IGN);
+	signal(SIGTERM, child_handler);
+	#endif
+
+    printf("child[%d]: I am child of %d process\n", (int) getpid(), (int) getppid());
+
+    sleep(10);
+
+    exit(0);
+}
+
+
 int main()
 {
+    pid_t pid;
+    int child_number = 0;
+    int child_executed = 0;
+    int NUM_CHILD = 5;
+    int pid_numbers [NUM_CHILD];
+    int status;
 
+    #ifdef WITH_SIGNALS
+	for(int i = 0; i < NSIG; i++)
+		signal(i, SIG_IGN);
 
-	int NUM_CHILD = 2;
-	int pid_numbers [NUM_CHILD];			
-	pid_t pid;
-	
-	for (int i = 0; i <= NUM_CHILD; i++)
-	{
-		pid = fork();
-		if (pid < 0)
-		{
-			printf("Not correctly created procces, terminating other proccess\n");
-		}
+	signal(SIGCHLD, SIG_DFL);
 
-		
-	//	printf("%d,ID: %d \n", i,getpid());
-	 	if((int) pid > 0)
-	 	{
+	signal(SIGINT, parent_handler);
+	#endif
+
+    for(int i=0;i<NUM_CHILD;i++){
+
+            #ifdef WITH_SIGNALS
+            if(set_signal_parent) {
+                printf("\nparent[%d]:  Terminating signal was caught by parent. Creation of new childs will be abandoned.\n", getpid());
+                for(int g=0; g<child_number;g++)
+                    kill(pid_numbers[g], SIGTERM);
+                break;
+            }
+            #endif
+
+            pid = fork();
+
+            printf("\n");
+
+            if((int) pid != 0){
                 pid_numbers[i] = (int) pid;
-                printf("rodzic[%d]: Dziecka pid: %d\n", (int) getpid(),(int) pid);
-      }		
-		sleep(1);	
-		
+                printf("parent[%d]: New child has been created with pid: %d\n", (int) getpid(),(int) pid);
+            }
+
+            if(pid < 0){
+                printf("child[%d]: fork() failed. Terminating all created child processess.", getpid());
+                for(int g=0; g<child_number;g++)
+                    kill(pid_numbers[g], SIGTERM);
+                exit(1);
+            }
+
+            if(pid == 0)
+               child_function();
+
+        sleep(1);
+        child_number++;
+    }
+
+    printf("\n\nparent[%d]: Waiting for child proccesses to be executed\n\n");
+
+    while((pid = wait(&status)) != -1) {
+        printf("parent[%d]: Child %d has been terminated.\n", (int) getpid(),(int) pid);
+        child_executed++;
 	}
 
-	
-	sleep(5);
-	return 0;
+    printf("\n\nparent[%d]: There are no more child processes. \n", (int) getpid());
+    printf("parent[%d]: Thre has been %d childs executed\n", (int) getpid(),child_executed);
+
+    #ifdef WITH_SIGNALS
+	for(int i = 0; i < NSIG; i++)
+		signal(i, SIG_DFL);
+	#endif
+
+    return 0;
 }
+
